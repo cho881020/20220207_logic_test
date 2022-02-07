7c4f83963aa491dbe3e02fecb595b536b8e0f85a
nepp.plus <nepp.plus@gmail.com>
2022-02-07 11:20:54 +0900

    국내 거래소의 API 활용
     JSON 응답 분석

--- test_03.py
+++ test_03.py
@@ -3,3 +3,11 @@
 # 한국 BTC 시세 Vs. 해외 BTC 시세 비교.
 import requests
 
+# 업비트의 현재 시세 조회 API 호출
+upbit_response = requests.get('https://api.upbit.com/v1/ticker?markets=KRW-BTC')
+
+upbit_response_json = upbit_response.json()
+
+upbit_btc_price = upbit_response_json[0]['trade_price']
+
+print(f'국내 Upbit의 BTC가격 : {upbit_btc_price}원')
