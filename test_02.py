160182fb4939e7385751642d0c4e37e8109f51af
nepp.plus <nepp.plus@gmail.com>
2022-02-07 10:42:34 +0900

    홀/짝에 따른 승패 결정
    중간 단계 출력도 같이 진행

--- test_02.py
+++ test_02.py
@@ -17,4 +17,32 @@ while True:
         continue  # 반복문의 이번 바퀴만 skip
         
     # 제대로 입력햇다면 마저 진행.
-    print('홀/짝 판별 차례')
+    
+    # CPU가 몇개를 집었는지 공개
+    print(f'CPU는 {cpu_count}개의 구슬을 집었습니다.')
+    
+    if user_answer == '홀':
+        if cpu_count % 2 == 1:
+            
+            print('사용자 승리입니다.')
+            
+            # 사용자 승리. => 코인 한개 CPU로부터 받아오자.
+            user_coin += 1
+            cpu_coin -= 1
+        else:
+            print('사용자 패배입니다.')
+            user_coin -= 1
+            cpu_coin += 1
+    else:
+        # 짝을 입력한 경우.
+        if cpu_count % 2 == 0:
+            
+            print('사용자 승리입니다.')
+            # 맞춘 경우.
+            user_coin += 1
+            cpu_coin -= 1
+        else:
+            
+            print('사용자 패배입니다.')
+            user_coin -= 1
+            cpu_coin += 1
